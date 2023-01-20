9e91713ebda0fd6f5d209fc32e4420af9e6bea10
maddayz <38384558+maddayz@users.noreply.github.com>
2023-01-20 06:41:12 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Grid from './Components/Grid'
 
 
 const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  NEXT_PUBLIC_SUPABASE_URL
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 
