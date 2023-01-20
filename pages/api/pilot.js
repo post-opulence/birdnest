165d427c624d9d578ecb4b9835f2dbee38467f84
maddayz <38384558+maddayz@users.noreply.github.com>
2023-01-20 07:07:45 +0200

    fix

--- pages/api/pilot.js
+++ pages/api/pilot.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default async function pilot(req, res) {
     const SN = req.query.SN
-    async function FetchData() {
+    async function fetchPilot() {
        return await axios.get(`https://assignments.reaktor.com/birdnest/pilots/${SN}`, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -11,7 +11,7 @@ export default async function pilot(req, res) {
         })
     }    
     try {
-       const pilot = await FetchData()
+       const pilot = await fetchPilot()
        res.status(200).send(pilot.data)
     }
     catch (error) {
