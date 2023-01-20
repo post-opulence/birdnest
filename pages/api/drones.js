8f631389278cfebe550877d29d489c9f7d4c6a56
maddayz <38384558+maddayz@users.noreply.github.com>
2023-01-20 07:34:02 +0200

    test

--- pages/api/drones.js
+++ pages/api/drones.js
@@ -1,59 +1,44 @@
 import axios from 'axios'
 import { XMLParser } from 'fast-xml-parser';
-import { Client } from 'pg';
+import { createClient } from '@supabase/supabase-js'
 var cron = require('node-cron'); 
 
-const client = new Client({
-    connectionString: process.env.CONNECTION_STRING,
-});
-
-client.connect();
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  )
+  
 
 
 export default async function drones(req, res) {
     
     async function storeData(drone, pilot) {
         try {
-          const existingDrone = await client.query(`SELECT * FROM drones WHERE serial_number = $1`, [drone.serialNumber]);
+          const existingDrone = await supabase.from('drones').select('*').where({ serial_number: drone.serialNumber });
           const distance = Math.round(Math.sqrt(Math.pow(drone.positionX - 250000, 2) + Math.pow(drone.positionY - 250000, 2)) / 1000);
-          const serial_number = drone.serialNumber;
-          const position_x = parseFloat(drone.positionX);
-          const position_y = parseFloat(drone.positionY);
-          console.log(pilot.createdDt)
-          const first_seen = new Date(pilot.createdDt).toISOString();
-          const pilot_id = pilot.pilotId
-          const first_name = pilot.firstName
-          const last_name = pilot.lastName
-          const phone_number = pilot.phoneNumber
-          const email = pilot.email
-          if (existingDrone.rows.length > 0) {
-            // The drone is already in the database, so we just need to update the data
-            await client.query(`UPDATE drones SET 
-                      position_x = $1, 
-                      position_y = $2, 
-                      distance = $3, 
-                      last_seen = now() 
-                      WHERE serial_number = $4`, [position_x, position_y, distance, serial_number]);
+          const data = {
+            serial_number: drone.serialNumber,
+            position_x: parseFloat(drone.positionX),
+            position_y: parseFloat(drone.positionY),
+            distance: distance,
+            first_seen: new Date(pilot.createdDt).toISOString(),
+            pilot_id: pilot.pilotId,
+            first_name: pilot.firstName,
+            last_name: pilot.lastName,
+            phone_number: pilot.phoneNumber,
+            email: pilot.email,
+          }
+      
+          if (existingDrone.length > 0) {
+            await supabase.from('drones').update(data).where({ serial_number: drone.serialNumber });
           } else {
-            // The drone is not in the database, so we need to insert it
-            await client.query(`INSERT INTO drones (
-                      serial_number, 
-                      position_x, 
-                      position_y, 
-                      distance,  
-                      first_seen,
-                      last_seen,
-                      pilot_id,
-                      first_name, 
-                      last_name,
-                      phone_number,
-                      email
-                  ) VALUES ($1, $2, $3, $4, to_timestamp($5,'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), now(), $6, $7, $8, $9, $10)`, [serial_number, position_x, position_y, distance, first_seen, pilot_id, first_name, last_name, phone_number, email]);  
-            }
+            await supabase.from('drones').insert(data);
+          }
         } catch (error) {
-            console.error(error);
+          console.error(error);
         }
-    }
+      }
+      
     // This function fetches the drone data from the Reaktor API, parses it into JSON and then sorts it. 
     async function fetchDrones() {
         const xmlData = await axios.get('https://assignments.reaktor.com/birdnest/drones', {
